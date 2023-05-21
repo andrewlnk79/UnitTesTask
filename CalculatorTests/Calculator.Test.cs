32db05b8b0a68cf5ffd18bda15feb0eff50f4b56
Andrey Linnik <av.linnik79@ya.ru>
2023-05-21 14:12:24 +0300

    Добавьте файлы проекта.

---
+++ CalculatorTests/Calculator.Test.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using UnitTesTask;
+
+namespace CalculatorTests
+{
+    [TestFixture]   
+    public class CalculatorTest
+
+    {
+       
+
+        [Test]
+        public void AdditionalExpectReturnSummOfTowNumbers()
+        {
+            var calc=new Calculator();
+            Assert.AreEqual(calc.Additional(5, 10), 15);
+        }
+        [Test]
+        public void SubtractionTwoNumbers()
+        {
+            var calc = new Calculator();
+            Assert.AreEqual(calc.Subtraction(10, 5), 5);
+        }
+        [Test]
+        public void MultiplicationTwoNumbers()
+        {
+            var calc = new Calculator();
+            Assert.AreNotEqual(calc.Miltiplication(10, 5), 5);
+        }
+        [Test]
+        public void DevisionTwoNumbers()
+        {
+            var calc = new Calculator();    
+            Assert.That(calc.Division(10,5), Is.EqualTo(2));
+            
+        }
+    }
+}
